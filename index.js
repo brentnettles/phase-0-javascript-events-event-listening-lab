92c79c2f69a3cdd0aeaf286ccb7f7b2dc6726b7c
Brent Nettles <BrentNettles@gmail.com>
2024-01-17 13:02:26 -0500

    Event Listening

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+
 function addingEventListener() {
-}
+    const buttonElement = document.getElementById('button');
+  
+    if (buttonElement) {
+      buttonElement.addEventListener('click', function() {
+        console.log('Button clicked!'); 
+      });
+    }
+  }
